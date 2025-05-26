a21b9f45461d6fa27211b537812d834e8b92e8c1
Delanie <degraham@pdx.edu>
2025-05-25 22:09:43 -0700

    Added another page for adding new events.

---
+++ client/src/components/pages/create.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Create = ()=>{
+    return (
+        <div className="card input-filed">
+            <input type="text" placeholder="Event name">
+            </input>
+        </div>
+    )
+}
